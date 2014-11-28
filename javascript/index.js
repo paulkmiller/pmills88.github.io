7f1f0e729658f4b27ffa39212a5f5c0018391a78
pmills88 <pmillerk88@gmail.com>
2014-11-28 11:36:26 -0500

    SASSified everything

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,17 @@
 $(function() {
+	// menu nav movement
   $("[href^='#']").on("click", function(e) {  
 
 	  var target = $(this).attr('href');
-	  var scrollTop = $( target ).offset().top-$('.header').height()-$('.header').outerHeight();
+	  var scrollTop = $(target).offset().top-$('.header').height()-$('.header').outerHeight();
 
-	  if ( target == '#top'){
+	  if (target == '#top'){
 	     scrollTop = 0;}
 	  $("body, html").animate({
 	          scrollTop: scrollTop
 	      }, 800);
-
 	  e.preventDefault();
   });
+
+  // hidden mousey message
 });
